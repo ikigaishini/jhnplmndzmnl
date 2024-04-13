fe691c185113a337876a789741d14841ef9419e8
ikigaishini <johnpaulmendozamanuel@yahoo.com>
2024-04-13 08:04:32 +0800

    Main

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <div class="container-fluid p-5 bg-primary text-white text-center">
-  <h1>My First Bootstrap Page</h1>
+  <h1>My First Bootrap Page</h1>
   <p>Resize this responsive page to see the effect!</p>
 </div>
 
